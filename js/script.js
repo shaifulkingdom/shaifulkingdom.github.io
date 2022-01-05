1b0155e05b03d554edba7314a436ce8fb6e05385
Shaiful Islam <shaifulkingdom@gmail.com>
2022-01-05 09:38:15 +0600

    Full Website
    Design Change

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 //=================================================================================//
 var typed = new Typed('.element', {
   strings: [
-    'I am a Web Designer',
+    'I am a Front-End Developer',
     'PSD To HTML5 Converter',
     'Problem Solver',
     'If you faced any code problem',
@@ -21,11 +21,11 @@ var typed = new Typed('.element', {
 //=================================================================================//
 
 //=================================================================================//
-//                                nav + main-part-start
+//                                nav-part-start
 //=================================================================================//
 
 $("nav ul li").on("click", "a", function () {
-  var position = $($(this).attr("href")).offset().top - 190;
+  var position = $($(this).attr("href")).offset().top - 50;
   $("html").animate({
     scrollTop: position
   }, 600);
@@ -34,92 +34,34 @@ $("nav ul li").on("click", "a", function () {
   event.preventDefault();
 });
 
-$("#banner-part .overlay .left").on("click", ".hireme", function () {
-  var position = $($(this).attr("href")).offset().top - 190;
+$("#about .about .right p").on("click", ".color", function () {
+  var position = $($(this).attr("href")).offset().top - 50;
   $("html").animate({
     scrollTop: position
   }, 600);
   event.preventDefault();
 });
 
-$("#main .about .article p").on("click", ".recentworks", function () {
-  var position = $($(this).attr("href")).offset().top - 190;
-  $("html").animate({
-    scrollTop: position
-  }, 600);
-  event.preventDefault();
-});
+// $("#banner-part .overlay .left").on("click", ".hireme", function () {
+//   var position = $($(this).attr("href")).offset().top - 190;
+//   $("html").animate({
+//     scrollTop: position
+//   }, 600);
+//   event.preventDefault();
+// });
 
-$("#main .offer .content p").on("click", ".recentworks", function () {
-  var position = $($(this).attr("href")).offset().top - 190;
-  $("html").animate({
-    scrollTop: position
-  }, 600);
-  event.preventDefault();
-});
-
-//=================================================================================//
-//                                Nav + main-part-end
-//=================================================================================//
-$('.moreless-button').click(function() {
-  $('.moretext').slideToggle();
-  if ($('.moreless-button').text() == "Read More") {
-    $(this).text("Close")
-  } else {
-    $(this).text("Read More")
-  }
-});
-//=================================================================================//
-//                                client-part-start
-//=================================================================================//
+// $("#main .about .article p").on("click", ".recentworks", function () {
+//   var position = $($(this).attr("href")).offset().top - 190;
+//   $("html").animate({
+//     scrollTop: position
+//   }, 600);
+//   event.preventDefault();
+// });
 
 
-$('.testimonials-wrapper').slick({
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplayspeed: 1000,
-  dots: true,
-  arrows: false,
-  responsive: [{
-      breakpoint: 1199.98,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 991.98,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 767.98,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 575.98,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
 
 //=================================================================================//
-//                                client-part-end
+//                                Nav-part-end
 //=================================================================================//
 
 //=================================================================================//
